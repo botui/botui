a3533bff4469466225e85913258ea379462feadf
Moin <me@moin.im>
2022-03-25 21:00:42 +0500

    init with control

--- src/scripts/botui.js
+++ src/scripts/botui.js
@@ -1,33 +1,92 @@
-(function (root, factory) {
-  "use strict";
-  if (typeof define === 'function' && define.amd) {
-    define([], function () {
-      return (root.BotUI = factory(root));
-    });
-  } else {
-    root.BotUI = factory(root);
+
+export const BOTUI_TYPES = {
+  WAIT: 'wait',
+  ACTION: 'action',
+  MESSAGE: 'message',
+}
+
+const createBlock = (type = '', meta = {}, data = {}) => {
+  return {
+    type: type,
+    meta: meta,
+    data: data
   }
-}(typeof window !== 'undefined' ? window : this, function (root, undefined) {
-  "use strict";
+}
 
-  var BotUI = (function (id, opts) {
+export const botuiControl = () => {
+  const history = []
+  let resolver = null
+  let currentAction = null
 
-    opts = opts || {};
+  let callback = () => {}
+  const doCallback = () => {
+    callback(currentAction, history)
+  }
 
-    if(!id) {
-      throw Error('BotUI: Container id is required as first argument.');
+  const doResolve = (...args) => {
+    resolver(...args)
+  }
+
+  const msg = {
+    add: (block) => {
+      // if (block?.meta?.ephemeral) return
+
+      const index = history.push(block)
+      doCallback()
+      return index - 1
+    },
+    update: (index, block) => {
+      console.log('update', index, history[index])
+
+      history[index] = block
+      doCallback()
+    },
+    remove: (index) => {
+      history.splice(index, 1)
+      doCallback()
     }
+  }
 
-    if(!document.getElementById(id)) {
-      throw Error('BotUI: Element with id #' + id + ' does not exist.');
+  return {
+    message: (meta = {}, data = { text: '' }) => {
+      return new Promise((resolve) => {
+        resolver = resolve
+        msg.add(createBlock(BOTUI_TYPES.MESSAGE, meta, data))
+        doResolve()
+      })
+    },
+    wait: (meta = { time: 0 }) => {
+      return new Promise((resolve) => {
+        resolver = resolve
+        meta.ephemeral = true
+        const index = msg.add(createBlock(BOTUI_TYPES.WAIT, meta))
+        setTimeout(() => {
+          msg.remove(index)
+          doResolve()
+        }, meta.time)
+      })
+    },
+    action: (meta = {}, data = { text: '' }) => {
+      return new Promise((resolve) => {
+        const action = createBlock(BOTUI_TYPES.ACTION, meta, data)
+
+        currentAction = action
+        const index = msg.add(action)
+
+        resolver = (...args) => {
+          currentAction = null
+          msg.update(index, createBlock(BOTUI_TYPES.MESSAGE, {
+            type: BOTUI_TYPES.ACTION
+          }, ...args))
+          resolve(...args)
+        }
+      })
+    },
+    onCallback: (cb = () => {}) => {
+      callback = cb
+    },
+    next: (...args) => {
+      doResolve(...args)
     }
-
-    var botApp, // current vue instance.
-    botInterface = {} // methods returned by a BotUI() instance.
-
-    return botInterface;
-  });
-
-  return BotUI;
-
-}));
+  }
+}
