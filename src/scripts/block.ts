8c54cfbe56f08a4e0c145de19c07227b0f2eebf3
Moin <me@moin.im>
2022-09-12 18:32:33 +0500

    add .key to block

--- src/scripts/block.ts
+++ src/scripts/block.ts
@@ -11,6 +11,7 @@ export type BlockData = object
 export type History = Block[]
 
 export interface Block {
+  key: number,
   type: string
   meta: BlockMeta
   data: BlockData
@@ -19,36 +20,45 @@ export interface Block {
 export interface BlockManager {
   add(data: BlockData, meta: BlockMeta): Promise<number>
   getAll(): Promise<Block[]>
-  get(index: number): Promise<Block>
-  remove(index: number): Promise<void>
-  update(index: number, block: Block): Promise<void>
+  get(key: number): Promise<Block>
+  remove(key: number): Promise<void>
+  update(key: number, data: BlockData, meta: BlockMeta): Promise<void>
   removeAll(): Promise<void>
 }
 
-export function createBlock(type: string, meta: BlockMeta, data: BlockData): Block {
+export function createBlock(type: string, meta: BlockMeta, data: BlockData, key?: number): Block {
   return {
+    key: key ?? -1,
     type: type,
     meta: meta,
     data: data,
   }
 }
 
+// it only manages the listed blocks in the UI, not the action.
 export function blockManager(callback = (history: History = []) => {}) {
   let history: History = []
+  const getBlockIndexByKey = (key = -1) => history.findIndex(block => block.key === key)
 
   return {
     getAll: () => history,
-    get: (index = 0) => history[index],
-    add: (block: Block): number => {
-      const length = history.push(block)
-      callback(history)
-      return length - 1
+    get: (key: number) => {
+      return history[getBlockIndexByKey(key)]
     },
-    update: (index: number, block: Block): void => {
+    add: (block: Block): number => {
+      const length = history.length
+      block.key = length
+      history.push(block)
+      callback(history)
+      return length
+    },
+    update: (key: number, block: Block): void => {
+      const index = getBlockIndexByKey(key)
       history[index] = block
       callback(history)
     },
-    remove: (index: number): void => {
+    remove: (key: number): void => {
+      const index = getBlockIndexByKey(key)
       history.splice(index, 1)
       callback(history)
     },
