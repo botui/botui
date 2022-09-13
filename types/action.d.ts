d17a63ab90e7ec53ce6a28bd15ebc820ce10622b
Moin <me@moin.im>
2022-09-13 17:29:06 +0500

    Update action.d.ts

--- types/action.d.ts
+++ types/action.d.ts
@@ -1,6 +1,5 @@
 import type { Block } from './block';
 export declare function actionManager(callback?: (action: Block | null) => void): {
-    get: () => Block;
     set: (action: Block) => void;
     clear: () => void;
 };
