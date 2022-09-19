9d6e195366bac2423ca765c7406f39e7d0499c61
Moin <me@moin.im>
2022-09-19 11:08:10 +0500

    Create BotUIActionSelect.tsx

---
+++ packages/with-react/src/BotUIActionSelect.tsx
@@ -0,0 +1,61 @@
+import { Block, BlockData } from 'botui'
+import React, { useState, useMemo } from 'react'
+import { useBotUI, useBotUIAction } from './hooks'
+
+export type ActionSelectOption = {
+  value: any
+  label: string
+  selected: boolean
+}
+
+export type ActionSelect = {
+  isMultiSelect?: boolean
+  options: ActionSelectOption[]
+}
+
+type ActionSelectBlock = Block & {
+  data: BlockData & ActionSelect
+}
+
+export const BotuiActionSelect = () => {
+  const bot = useBotUI()
+  const action = useBotUIAction() as ActionSelectBlock
+
+  const defaultSelection =
+    action?.data.options.findIndex((option) => option.selected) ?? 0
+  const [selected, setSelected] = useState(defaultSelection)
+  const selectedObject = useMemo(
+    () => action?.data.options[selected],
+    [selected]
+  )
+
+  return (
+    <div className="botui-action core-select">
+      <select
+        value={selected}
+        multiple={action.data.isMultiSelect}
+        onChange={(e) => {
+          setSelected(parseInt(e.target.value))
+        }}
+      >
+        {action?.data.options.map((opt, i) => (
+          <option className='core-select-option' key={opt.value} value={i}>
+            {opt.label || opt.value}
+          </option>
+        ))}
+      </select>
+
+      <button
+        className='botui-button'
+        onClick={() =>
+          bot.next({
+            selected: selectedObject,
+            label: selectedObject.label || selectedObject.value,
+          })
+        }
+      >
+        Done
+      </button>
+    </div>
+  )
+}
