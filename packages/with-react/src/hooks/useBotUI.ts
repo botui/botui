33c1053a0c7d76ae6c91f42478a0131ba07300ec
Moin <me@moin.im>
2022-09-24 13:12:01 +0500

    add useBotTheme hook

--- packages/with-react/src/hooks/useBotUI.ts
+++ packages/with-react/src/hooks/useBotUI.ts
@@ -1,17 +1,36 @@
 import { BotuiInterface } from 'botui'
 import { useContext, createContext } from 'react'
+import { CSSClasses } from '../types'
 
-export const BotUIContext = createContext<BotuiInterface>({} as BotuiInterface)
+type BotContext = {
+  theme: CSSClasses
+  bot: BotuiInterface
+}
+
+export const BotUIContext = createContext<BotContext>({} as BotContext)
 
 /**
  * Returns the current botui object from the React context.
  */
 export const useBotUI = (): BotuiInterface => {
-  const context = useContext<BotuiInterface>(BotUIContext)
+  const context = useContext<BotContext>(BotUIContext)
   if (!context) {
     throw new Error(
       `The \`useBotUI\` hook must be used inside the <BotUI> component's context.`
     )
   }
-  return context
+  return context.bot
+}
+
+/**
+ * Returns the current botui theme object from the React context.
+ */
+export const useBotTheme = (): CSSClasses => {
+  const context = useContext<BotContext>(BotUIContext)
+  if (!context) {
+    throw new Error(
+      `The \`useBotUI\` hook must be used inside the <BotUI> component's context.`
+    )
+  }
+  return context.theme
 }
