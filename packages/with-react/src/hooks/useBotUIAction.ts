9239c102fa2009aca709fd33475c9503f85fe1a2
Moin <me@moin.im>
2022-09-24 10:01:34 +0500

    remove unnecessary check

--- packages/with-react/src/hooks/useBotUIAction.ts
+++ packages/with-react/src/hooks/useBotUIAction.ts
@@ -7,11 +7,5 @@ export const ActionContext = createContext<Block | null>(null)
  * Returns the current action
  */
 export const useBotUIAction = (): Block | null => {
-  const context = useContext(ActionContext)
-  if (!context) {
-    throw new Error(
-      `The \`useBotUIAction\` hook must be used inside the <BotUI> component's context.`
-    )
-  }
-  return context
+  return useContext(ActionContext)
 }
--- packages/with-react/src/hooks/useBotUIMessage.ts
+++ packages/with-react/src/hooks/useBotUIMessage.ts
@@ -7,11 +7,5 @@ export const MessageContext = createContext<Block[] | []>([])
  * Returns all the messages
  */
 export const useBotUIMessage = (): Block[] | [] => {
-  const context = useContext(MessageContext)
-  if (!context) {
-    throw new Error(
-      `The \`useBotUIMessage\` hook must be used inside the <BotUI> component's context.`
-    )
-  }
-  return context
+  return useContext(MessageContext)
 }
