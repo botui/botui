1757d0624664b080268c755d875534623a74f574
Moin <me@moin.im>
2022-09-24 13:36:33 +0500

    add more css classes

--- packages/with-react/src/types.ts
+++ packages/with-react/src/types.ts
@@ -2,6 +2,9 @@ export type CSSClasses = {
   botui_action: string
   botui_message: string
   botui_container: string
+  botui_message_list: string
   botui_app_container: string
+  botui_message_content: string
+  botui_action_container: string
   botui_message_container: string
 }
