b1bd56f637216fa9524c2ef9bc7d3aa4b34c44bc
Moin <me@moin.im>
2022-09-25 14:50:37 +0500

    updated scss

--- packages/with-react/src/styles/default.theme.module.scss.d.ts
+++ packages/with-react/src/styles/default.theme.module.scss.d.ts
@@ -1,5 +1,17 @@
-import { CSSClasses } from "../types";
+export type Styles = {
+  'botui_action': string;
+  'botui_action_container': string;
+  'botui_app_container': string;
+  'botui_container': string;
+  'botui_message': string;
+  'botui_message_container': string;
+  'botui_message_content': string;
+  'botui_message_list': string;
+  'human': string;
+};
 
-declare module "default.theme.module.scss" {
-  export = CSSClasses;
-}
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
